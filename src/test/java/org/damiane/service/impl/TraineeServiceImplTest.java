c6e80e2e39dd2d2c68ffdcd2e8b34b274d024d79
NavrozashviliDamiane <navrozashvili1997@gmail.com>
2024-03-13 16:56:40 +0500

    Added Trainer unit tests and changed name conventions of the tests

--- src/test/java/org/damiane/service/impl/TraineeServiceImplTest.java
+++ src/test/java/org/damiane/service/impl/TraineeServiceImplTest.java
@@ -4,7 +4,6 @@ import org.damiane.entity.Trainee;
 import org.damiane.entity.User;
 import org.damiane.repository.TraineeRepository;
 import org.damiane.service.AuthenticateService;
-import org.damiane.service.TraineeService;
 import org.damiane.service.TrainingService;
 import org.damiane.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,17 +43,14 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_ValidCredentials_When_GetAllTrainees_Then_ReturnListOfTrainees() {
-        // Given
         String username = "testUser";
         String password = "testPassword";
         List<Trainee> expectedTrainees = new ArrayList<>();
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(traineeRepository.findAll()).thenReturn(expectedTrainees);
 
-        // When
         List<Trainee> actualTrainees = traineeService.getAllTrainees(username, password);
 
-        // Then
         verify(authenticateService).matchUserCredentials(username, password);
         verify(traineeRepository).findAll();
         assertEquals(expectedTrainees, actualTrainees);
@@ -63,17 +59,14 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_ValidCredentials_When_GetTraineeByUsername_Then_ReturnTrainee() {
-        // Given
         String username = "testUser";
         String password = "testPassword";
         Trainee expectedTrainee = new Trainee();
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(traineeRepository.findByUserUsername(username)).thenReturn(expectedTrainee);
 
-        // When
         Trainee actualTrainee = traineeService.getTraineeByUsername(username, password);
 
-        // Then
         verify(authenticateService).matchUserCredentials(username, password);
         verify(traineeRepository).findByUserUsername(username);
         assertEquals(expectedTrainee, actualTrainee);
@@ -81,7 +74,6 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_ValidCredentialsAndNewPassword_When_ChangeTraineePassword_Then_PasswordIsChanged() {
-        // Given
         Long traineeId = 1L;
         String username = "testUser";
         String password = "testPassword";
@@ -91,10 +83,8 @@ class TraineeServiceImplTest {
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(traineeRepository.findById(traineeId)).thenReturn(java.util.Optional.of(trainee));
 
-        // When
         traineeService.changeTraineePassword(traineeId, username, password, newPassword);
 
-        // Then
         verify(authenticateService).matchUserCredentials(username, password);
         verify(traineeRepository).findById(traineeId);
         assertEquals(newPassword, trainee.getUser().getPassword());
@@ -102,7 +92,6 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_TraineeData_When_CreateTrainee_Then_ReturnCreatedTrainee() {
-        // Given
         String firstName = "John";
         String lastName = "Doe";
         Date dateOfBirth = new Date();
@@ -112,10 +101,8 @@ class TraineeServiceImplTest {
         when(userService.createUser(firstName, lastName)).thenReturn(createdTrainee.getUser());
         when(traineeRepository.save(any())).thenReturn(createdTrainee);
 
-        // When
         Trainee actualTrainee = traineeService.createTrainee(firstName, lastName, dateOfBirth, address);
 
-        // Then
         verify(userService).createUser(firstName, lastName);
         verify(traineeRepository).save(any());
         assertEquals(createdTrainee, actualTrainee);
@@ -123,7 +110,6 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_ValidCredentials_When_ActivateTrainee_Then_TraineeIsActive() {
-        // Given
         Long traineeId = 1L;
         String username = "testUser";
         String password = "testPassword";
@@ -132,10 +118,8 @@ class TraineeServiceImplTest {
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(traineeRepository.findById(traineeId)).thenReturn(java.util.Optional.of(trainee));
 
-        // When
         traineeService.activateTrainee(traineeId, username, password);
 
-        // Then
         verify(authenticateService).matchUserCredentials(username, password);
         verify(traineeRepository).findById(traineeId);
         assertTrue(trainee.getUser().isActive());
@@ -143,7 +127,6 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_ValidCredentials_When_DeactivateTrainee_Then_TraineeIsInactive() {
-        // Given
         Long traineeId = 1L;
         String username = "testUser";
         String password = "testPassword";
@@ -152,10 +135,8 @@ class TraineeServiceImplTest {
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(traineeRepository.findById(traineeId)).thenReturn(java.util.Optional.of(trainee));
 
-        // When
         traineeService.deactivateTrainee(traineeId, username, password);
 
-        // Then
         verify(authenticateService).matchUserCredentials(username, password);
         verify(traineeRepository).findById(traineeId);
         assertFalse(trainee.getUser().isActive());
@@ -163,7 +144,6 @@ class TraineeServiceImplTest {
 
     @Test
     void Given_ValidCredentials_When_DeleteTraineeByUsername_Then_TraineeIsDeleted() {
-        // Given
         String username = "testUser";
         String password = "testPassword";
         Trainee trainee = new Trainee();
@@ -171,10 +151,8 @@ class TraineeServiceImplTest {
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(traineeRepository.findByUserUsername(username)).thenReturn(trainee);
 
-        // When
         traineeService.deleteTraineeByUsername(username, password);
 
-        // Then
         verify(authenticateService).matchUserCredentials(username, password);
         verify(trainingService).updateTrainingForTrainee(username);
         verify(traineeRepository).findByUserUsername(username);
--- src/test/java/org/damiane/service/impl/TrainerServiceImplTest.java
+++ src/test/java/org/damiane/service/impl/TrainerServiceImplTest.java
@@ -1,8 +1,9 @@
 package org.damiane.service.impl;
 
-import org.damiane.entity.Trainer;
-import org.damiane.entity.User;
+import org.damiane.entity.*;
+import org.damiane.repository.TraineeRepository;
 import org.damiane.repository.TrainerRepository;
+import org.damiane.repository.TrainingRepository;
 import org.damiane.repository.TrainingTypeRepository;
 import org.damiane.service.AuthenticateService;
 import org.damiane.service.UserService;
@@ -12,10 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.*;
 
 class TrainerServiceImplTest {
@@ -30,69 +33,161 @@ class TrainerServiceImplTest {
     private TrainingTypeRepository trainingTypeRepository;
 
     @Mock
+    private TraineeRepository traineeRepository;
+
+    @Mock
+    private TrainingRepository trainingRepository;
+
+    @Mock
     private AuthenticateService authenticateService;
 
     @InjectMocks
     private TrainerServiceImpl trainerService;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void testGetTrainerByUsername() {
+    void getTrainerByUsername_Should_ReturnCorrectTrainer_When_ValidUsernameAndPassword() {
         String username = "testUser";
         String password = "testPassword";
         Trainer expectedTrainer = new Trainer();
-
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(trainerRepository.findByUserUsername(username)).thenReturn(expectedTrainer);
 
         Trainer actualTrainer = trainerService.getTrainerByUsername(username, password);
 
-        assertEquals(expectedTrainer, actualTrainer);
         verify(authenticateService).matchUserCredentials(username, password);
         verify(trainerRepository).findByUserUsername(username);
+        assertEquals(expectedTrainer, actualTrainer);
     }
 
     @Test
-    void testChangeTrainerPassword() {
+    void changeTrainerPassword_Should_ChangePassword_When_ValidTrainerIdAndCredentials() {
         Long trainerId = 1L;
         String username = "testUser";
         String password = "testPassword";
-        String newPassword = "newTestPassword";
-
+        String newPassword = "newPassword";
         Trainer trainer = new Trainer();
-        User user = new User();
-        user.setPassword(password);
-        trainer.setUser(user);
-
+        trainer.setUser(new User());
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(trainerRepository.findById(trainerId)).thenReturn(Optional.of(trainer));
 
         trainerService.changeTrainerPassword(trainerId, username, password, newPassword);
 
-        assertEquals(newPassword, trainer.getUser().getPassword());
         verify(authenticateService).matchUserCredentials(username, password);
         verify(trainerRepository).findById(trainerId);
-        verify(userService).saveUser(any());
+        assertEquals(newPassword, trainer.getUser().getPassword());
     }
 
     @Test
-    void testGetTrainerById() {
+    void getAllTrainers_Should_ReturnAllTrainers_When_ValidUsernameAndPassword() {
+        String username = "testUser";
+        String password = "testPassword";
+        List<Trainer> expectedTrainers = new ArrayList<>();
+        when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
+        when(trainerRepository.findAll()).thenReturn(expectedTrainers);
+
+        List<Trainer> actualTrainers = trainerService.getAllTrainers(username, password);
+
+        verify(authenticateService).matchUserCredentials(username, password);
+        verify(trainerRepository).findAll();
+        assertEquals(expectedTrainers, actualTrainers);
+    }
+
+    @Test
+    void getTrainerById_Should_ReturnCorrectTrainer_When_ValidIdUsernameAndPassword() {
         Long trainerId = 1L;
         String username = "testUser";
         String password = "testPassword";
         Trainer expectedTrainer = new Trainer();
-
         when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
         when(trainerRepository.findById(trainerId)).thenReturn(Optional.of(expectedTrainer));
 
         Trainer actualTrainer = trainerService.getTrainerById(trainerId, username, password);
 
-        assertEquals(expectedTrainer, actualTrainer);
         verify(authenticateService).matchUserCredentials(username, password);
         verify(trainerRepository).findById(trainerId);
+        assertEquals(expectedTrainer, actualTrainer);
     }
+
+    @Test
+    void updateTrainerProfile_Should_UpdateTrainerProfile_When_ValidUsernameAndPassword() {
+        String username = "testUser";
+        String password = "testPassword";
+        String firstName = "John";
+        String lastName = "Doe";
+        TrainingType trainingType = new TrainingType();
+        Trainer existingTrainer = new Trainer();
+        existingTrainer.setUser(new User());
+
+        when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
+        when(trainerRepository.findByUserUsername(username)).thenReturn(existingTrainer);
+        when(trainingTypeRepository.findByTrainingType(any())).thenReturn(trainingType);
+        when(trainerRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        Trainer updatedTrainer = trainerService.updateTrainerProfile(username, password, firstName, lastName, trainingType.getTrainingType());
+
+        assertEquals(firstName, updatedTrainer.getUser().getFirstName());
+        assertEquals(lastName, updatedTrainer.getUser().getLastName());
+        assertEquals(trainingType, updatedTrainer.getTrainingType());
+    }
+
+    @Test
+    void createTrainer_Should_CreateTrainer_When_ValidInput() {
+        String firstName = "John";
+        String lastName = "Doe";
+        TrainingType trainingType = new TrainingType(TrainingTypeValue.CARDIO);
+        Trainer createdTrainer = new Trainer();
+        createdTrainer.setUser(new User());
+        when(userService.createUser(firstName, lastName)).thenReturn(new User());
+        when(trainingTypeRepository.findByTrainingType(trainingType.getTrainingType())).thenReturn(trainingType);
+        when(trainerRepository.save(any())).thenReturn(createdTrainer);
+
+        Trainer actualTrainer = trainerService.createTrainer(firstName, lastName, trainingType);
+
+        verify(userService).createUser(firstName, lastName);
+        verify(trainingTypeRepository).findByTrainingType(trainingType.getTrainingType());
+        verify(trainerRepository).save(any());
+        assertEquals(createdTrainer, actualTrainer);
+    }
+
+
+    @Test
+    void deactivateTrainer_Should_DeactivateTrainer_When_ValidUsernameAndPassword() {
+        Long trainerId = 1L;
+        String username = "testUser";
+        String password = "testPassword";
+        Trainer trainer = new Trainer();
+        trainer.setUser(new User());
+
+        when(authenticateService.matchUserCredentials(username, password)).thenReturn(true);
+        when(trainerRepository.findById(trainerId)).thenReturn(Optional.of(trainer));
+
+        trainerService.deactivateTrainer(trainerId, username, password);
+
+        assertFalse(trainer.getUser().isActive());
+        verify(trainerRepository, times(1)).save(any());
+    }
+
+    @Test
+    void findUnassignedTrainersByTraineeUsername_Should_ReturnUnassignedTrainers_When_ValidUsernameAndPassword() {
+        String traineeUsername = "testUser";
+        String password = "testPassword";
+        Trainee trainee = new Trainee();
+        trainee.setUser(new User());
+        List<Trainer> allTrainers = new ArrayList<>();
+        List<Training> trainingsWithTrainee = new ArrayList<>();
+
+        when(authenticateService.matchUserCredentials(traineeUsername, password)).thenReturn(true);
+        when(traineeRepository.findByUserUsername(traineeUsername)).thenReturn(trainee);
+        when(trainerRepository.findAll()).thenReturn(allTrainers);
+        when(trainingRepository.findByTraineeId(any())).thenReturn(trainingsWithTrainee);
+
+        List<Trainer> unassignedTrainers = trainerService.findUnassignedTrainersByTraineeUsername(traineeUsername, password);
+
+    }
+
 }
